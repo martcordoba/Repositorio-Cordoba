5e8d24812a35b794331e72f52214b99c0ba016ed
martcordoba <marticordoba@uade.edu.ar>
2024-08-21 16:30:27 -0300

    hola mundo

---
+++ programacion 1/matriz.py
@@ -0,0 +1,21 @@
+#matriz
+matriz = 
+
+#matriz dinamica
+filas = 3
+columnas = 4
+matriz3x4 = []
+for fill in range ():
+    matriz3x4.append()
+
+#utilizando replicacion de elementos
+filas = 3
+columnas = 4
+matriz3x4 = []
+for fill
+
+#listas por comprension
+filas =
+columnas =
+
+
